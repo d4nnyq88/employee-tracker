89e42ed9f9e8f925a067fd5e2c589c0f372e6be2
Captain America <82297346+d4nnyq88@users.noreply.github.com>
2021-09-24 00:47:49 -0400

    added all functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const db = require('./server');
-
+//function that runs when the app is initialized and lists out actions for the user to select
 init = () => {
     inquirer
         .prompt([
@@ -46,11 +46,14 @@ init = () => {
                     break;
                 case "Quit":
                     console.log("Exiting Employee Tracker.");
-                    break;               
+                    break;
+                default:
+                    console.log("Something went wrong.");
+                    break;
             }
         });
 };
-
+//function to view all departments in the database
 function viewAllDepartments() {
     db.query(`SELECT * FROM departments`, (err, rows) => {
         if (err) {
@@ -61,7 +64,7 @@ function viewAllDepartments() {
         init();
       });
 }
-
+//function to view all roles in the database
 function viewAllRoles() {
     db.query(`SELECT roles.id, roles.title, roles.salary, departments.department_name AS department FROM roles JOIN departments ON departments.id = roles.department_id`, (err, rows) => {
         if (err) {
@@ -72,7 +75,7 @@ function viewAllRoles() {
         init();
       });
 }
-
+//function to view all employees in the database
 function viewAllEmployees() {
     db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(e.first_name, ' ', e.last_name) AS manager FROM employees LEFT JOIN roles ON roles.id = employees.role_id LEFT JOIN departments ON departments.id = roles.department_id LEFT JOIN employees e ON e.id = employees.manager_id`, (err, rows) => {
         if (err) {
@@ -83,7 +86,7 @@ function viewAllEmployees() {
         init();
     });
 }
-
+//function to add a new department to the departments table in the database
 function addDepartment() {
     inquirer
         .prompt([
@@ -109,16 +112,11 @@ function addDepartment() {
             });
         });
 }
-
+//function to add a new role to the roles table in the database
 function addRole() {
     getDepartments().then( ([rows, fields]) => {
         
-        console.log(rows);
-        
         const departmentList = rows.map(({ id, department_name }) => ({ name: department_name}));
-
-        console.log(departmentList);
-        console.log(typeof departmentList);
         //will get an array of rows back
 
         inquirer
@@ -172,19 +170,7 @@ function addRole() {
         });        
     })    
 }
-
-function getDepartments() {
-    return db.promise().query(`SELECT department_name FROM departments`);
-}
-
-function getManagers() {
-    return db.promise().query(`SELECT CONCAT(first_name,' ', last_name) AS manager_name FROM employees WHERE manager_id IS NULL`);
-}
-
-function getRoles() {
-    return db.promise().query(`SELECT title FROM roles`);
-}
-
+//function to add a new employee to the employees table in the database
 async function addEmployee() {
     
     const managerList = await getManagers();
@@ -192,16 +178,8 @@ async function addEmployee() {
     const departmentList = await getDepartments();
     const formattedDepartmentList = departmentList[0].map(({ department_name }) => ({ name: department_name}));
     const roleList = await getRoles();    
-    const formattedRoleList = roleList[0].map(({ title }) => ({ title: title}));
-
+    const formattedRoleList = roleList[0].map(({ title }) => ({ name: title}));
         
-    console.log('managerList: ', managerList);
-    console.log('formattedManagerList: ', formattedManagerList);
-    console.log('departmentList: ', departmentList);
-    console.log('formattedDepartmentList: ', formattedDepartmentList);
-    console.log('roleList: ', roleList);
-    console.log('formattedRoleList: ', formattedRoleList);
-
     const userInput = await inquirer
     .prompt([
         {
@@ -218,7 +196,7 @@ async function addEmployee() {
         {
             type: "input",
             name: "lastName",
-            message: "What is the last name of the employee",
+            message: "What is the last name of the employee?",
             validate: answer => {
                 if (answer !== "") {
                     return true;
@@ -245,36 +223,97 @@ async function addEmployee() {
             choices: formattedManagerList,
         },
     ]);
-    console.log('userInput: ', userInput);
 
-    const roleId = db.query(`SELECT id FROM roles WHERE title = ('${userInput.title}')`, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }        
-    });
-    
-    const roleId = db.query(`SELECT id FROM employees WHERE manager = ('${userInput.title}')`, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }        
-    });
-
-    db.query(`INSERT INTO employees(first_name, last_name, salary, department_id) VALUES('${userInput.roleName}','${userInput.salary}', '${deptId}')`, (err, result) => {
-        
+    db.query(`SELECT id FROM roles WHERE title = ('${userInput.title}')`, (err, result) => {
         if (err) {
             console.error(err);
             return;
         }
-        console.log(`Added '${userInput.roleName}' to the database`);
-        init();
+        const roleId = result[0].id;
+        
+        db.query(`SELECT id FROM employees WHERE first_name = ('${userInput.managerName.split(' ')[0]}') AND last_name = ('${userInput.managerName.split(' ')[1]}')`, (err, result) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const managerId = result[0].id;  
+
+            db.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('${userInput.firstName}','${userInput.lastName}', '${roleId}', '${managerId}')`, (err, result) => {
+        
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(`Added '${userInput.firstName}' '${userInput.lastName}' to the database`);
+                init();
+            });
+        });        
     });
-                    
 }
+//function to update an employees role in the employees table in the database
+async function updateEmployeeRole() {
+    const employeeList = await getEmployees();
+    const formattedEmployeeList = employeeList[0].map(({ employee_name }) => ({ name: employee_name}));
+    const roleList = await getRoles();    
+    const formattedRoleList = roleList[0].map(({ title }) => ({ name: title}));
 
-// function updateEmployeeRole() {
-    
-// }
+    const userInput = await inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "employeeName",
+            message: "Which employee's role do you want to update?",
+            choices: formattedEmployeeList,
+        },
+        {
+            type: "list",
+            name: "roleName",
+            message: "Which role do you want to assign the selected employee?",
+            choices: formattedRoleList,
+        },        
+    ]);
 
+    db.query(`SELECT id FROM roles WHERE title = ('${userInput.roleName}')`, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const roleId = result[0].id;
+        
+        db.query(`SELECT id FROM employees WHERE CONCAT(first_name, ' ', last_name) = ('${userInput.employeeName}')`, (err, result) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const employeeId = result[0].id;  
+
+            db.query(`UPDATE employees SET role_id = ? WHERE id =?`, [roleId, employeeId], (err, result) => {
+        
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(`Updated the role of '${userInput.employeeName}' to '${userInput.roleName}' in the database`);
+                init();
+            });
+        });        
+    });
+}
+//function to get a list of all existing departments in the database
+function getDepartments() {
+    return db.promise().query(`SELECT department_name FROM departments`);
+}
+//function to get a list of all existing managers in the database
+function getManagers() {
+    return db.promise().query(`SELECT CONCAT(first_name,' ', last_name) AS manager_name FROM employees WHERE manager_id IS NULL`);
+}
+//function to get a list of all existing roles in the database
+function getRoles() {
+    return db.promise().query(`SELECT title FROM roles`);
+}
+//function to get a list of all existing employees in the database
+function getEmployees() {
+    return db.promise().query(`SELECT CONCAT(first_name,' ',last_name) AS employee_name FROM employees`);
+}
+//initializes the app
 init();
